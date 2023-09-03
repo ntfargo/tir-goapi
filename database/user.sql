fa2685043a7fcf44eb337b4243d0131308ab1221
Nathan Fargo <32229490+ntfargo@users.noreply.github.com>
2023-09-03 07:06:05 +0200

    chk_password_length fix

--- database/user.sql
+++ database/user.sql
@@ -10,7 +10,7 @@ CREATE TABLE users (
     created_at TIMESTAMPTZ DEFAULT NOW(),
     last_login TIMESTAMPTZ,
     CONSTRAINT chk_role CHECK (role IN ('ADMIN', 'MODERATOR', 'MEMBER')),
-    CONSTRAINT chk_password_length CHECK (LENGTH(password) >= 8),
+    CONSTRAINT chk_password_length CHECK (LENGTH(password) >= 10),
     CONSTRAINT chk_full_name_length CHECK (LENGTH(full_name) <= 100),
     CONSTRAINT chk_email_format CHECK (email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}$'),
     CONSTRAINT chk_username_format CHECK (username ~* '^[A-Za-z0-9_]+$')
